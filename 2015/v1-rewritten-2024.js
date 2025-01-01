c93d6de8a78c78d26408db9083de8e2bec3c640d
Callum Fisher <cf.fisher.bham@gmail.com>
2025-01-01 17:04:33 +0000

    Standardize tabs.

--- 2015/v1-rewritten-2024.js
+++ 2015/v1-rewritten-2024.js
@@ -21,43 +21,43 @@
 // ==/UserScript==
 
 /*
-    "EpicOS (v1) - Rewritten"
-    2024.12.21 - 2024.12.31
+	"EpicOS (v1) - Rewritten"
+	2024.12.21 - 2024.12.31
 
-    This is a rewrite of the original 2015 web browser version of EpicOS. I have:
-    - Compiled separate message listeners into one
-    - Changed the command handler to a switch statement
-    - Removed unnecessary type coercion
+	This is a rewrite of the original 2015 web browser version of EpicOS. I have:
+	- Compiled separate message listeners into one
+	- Changed the command handler to a switch statement
+	- Removed unnecessary type coercion
 	
-    Userscript URL: https://greasyfork.org/scripts/521353
-    A repository of all published EpicOS projects is available: https://github.com/slowstone72/EpicOS
+	Userscript URL: https://greasyfork.org/scripts/521353
+	A repository of all published EpicOS projects is available: https://github.com/slowstone72/EpicOS
 
-    Callum Fisher <cf.fisher.bham@gmail.com>
+	Callum Fisher <cf.fisher.bham@gmail.com>
 
-    This is free and unencumbered software released into the public domain.
+	This is free and unencumbered software released into the public domain.
 
-    Anyone is free to copy, modify, publish, use, compile, sell, or
-    distribute this software, either in source code form or as a compiled
-    binary, for any purpose, commercial or non-commercial, and by any
-    means.
+	Anyone is free to copy, modify, publish, use, compile, sell, or
+	distribute this software, either in source code form or as a compiled
+	binary, for any purpose, commercial or non-commercial, and by any
+	means.
 
-    In jurisdictions that recognize copyright laws, the author or authors
-    of this software dedicate any and all copyright interest in the
-    software to the public domain. We make this dedication for the benefit
-    of the public at large and to the detriment of our heirs and
-    successors. We intend this dedication to be an overt act of
-    relinquishment in perpetuity of all present and future rights to this
-    software under copyright law.
+	In jurisdictions that recognize copyright laws, the author or authors
+	of this software dedicate any and all copyright interest in the
+	software to the public domain. We make this dedication for the benefit
+	of the public at large and to the detriment of our heirs and
+	successors. We intend this dedication to be an overt act of
+	relinquishment in perpetuity of all present and future rights to this
+	software under copyright law.
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-    OTHER DEALINGS IN THE SOFTWARE.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
 
-    For more information, please refer to <https://unlicense.org/>
+	For more information, please refer to <https://unlicense.org/>
 */
 
 let startEOS = () => {
